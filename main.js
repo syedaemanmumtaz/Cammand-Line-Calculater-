511ffcb67392632c1984be0654a56afe8be870ae
syedaemanmumtaz <158057266+syedaemanmumtaz@users.noreply.github.com>
2024-05-15 19:18:42 +0500

    TS calculater

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number :", type: "number ", name: "fristNumber" },
+    { message: "Enter first number :", type: "number ", name: "firstNumber" },
     { message: "Enter second number :", type: "number", name: "secondNumber" },
     {
         message: "select one of the operators to perform operation",
@@ -12,16 +13,16 @@ const answer = await inquirer.prompt([
 console.log(answer);
 //conditional statemet
 if (answer.operator === "Addition") {
-    console.log(answer.fristNumber + answer.secondNumber);
+    console.log(answer.firstNumber + answer.secondNumber);
 }
 else if (answer.operator === "Subraction") {
-    console.log(answer.fristNumber - answer.secondNumber);
+    console.log(answer.firstNumber - answer.secondNumber);
 }
 else if (answer.operator === "Multiplication") {
-    console.log(answer.fristNumber * answer.secondNumber);
+    console.log(answer.firstNumber * answer.secondNumber);
 }
 else if (answer.operator === "Division") {
-    console.log(answer.fristNumber / answer.secondNumber);
+    console.log(answer.firstNumber / answer.secondNumber);
 }
 else {
     console.log("Please select valid operator");
--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 import inquirer from "inquirer";
 
 const  answer = await inquirer.prompt([
-        {message: "Enter first number :", type:"number ", name:"fristNumber"},
+        {message: "Enter first number :" , type:"number ", name:"firstNumber"},
         {message: "Enter second number :", type: "number", name: "secondNumber"},
         {
             message: "select one of the operators to perform operation",
@@ -16,13 +16,13 @@ console.log(answer);
 
 //conditional statemet
 if (answer.operator === "Addition"){
-    console.log( answer.fristNumber + answer.secondNumber);
+    console.log( answer.firstNumber + answer.secondNumber);
 } else if(answer.operator === "Subraction"){
-    console.log( answer.fristNumber - answer.secondNumber);
+    console.log( answer.firstNumber - answer.secondNumber);
 }else if(answer.operator === "Multiplication"){
-    console.log( answer.fristNumber * answer.secondNumber);
+    console.log( answer.firstNumber * answer.secondNumber);
 }else if(answer.operator === "Division"){
-    console.log( answer.fristNumber / answer.secondNumber);
+    console.log( answer.firstNumber / answer.secondNumber);
 }else{
     console.log("Please select valid operator");
 }
