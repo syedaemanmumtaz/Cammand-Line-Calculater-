697669a0ce8e34d321713d4febcf6076a80ea6c9
syedaemanmumtaz <158057266+syedaemanmumtaz@users.noreply.github.com>
2024-05-15 17:47:24 +0500

    TS calculater commit

---
+++ main.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+
+const  answer = await inquirer.prompt([
+        {message: "Enter first number :", type:"number ", name:"fristNumber"},
+        {message: "Enter second number :", type: "number", name: "secondNumber"},
+        {
+            message: "select one of the operators to perform operation",
+            type: "list",
+            name: "operator",
+            choices: ["Addition", "Subraction", "Multiplication", "Division"]
+        },
+]);
+console.log(answer);
+
+//conditional statemet
+if (answer.operator === "Addition"){
+    console.log( answer.fristNumber + answer.secondNumber);
+} else if(answer.operator === "Subraction"){
+    console.log( answer.fristNumber - answer.secondNumber);
+}else if(answer.operator === "Multiplication"){
+    console.log( answer.fristNumber * answer.secondNumber);
+}else if(answer.operator === "Division"){
+    console.log( answer.fristNumber / answer.secondNumber);
+}else{
+    console.log("Please select valid operator");
+}
